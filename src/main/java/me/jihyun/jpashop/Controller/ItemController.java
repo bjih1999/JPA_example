e7aff34ac729c58598a1469d219e057cc7e0dd78
Jihyun <bjih1999@naver.com>
2021-08-31 23:37:19 +0900

    feat: 상품 등록

--- src/main/java/me/jihyun/jpashop/Controller/ItemController.java
+++ src/main/java/me/jihyun/jpashop/Controller/ItemController.java
@@ -1,2 +1,40 @@
-package me.jihyun.jpashop.Controller;public class ItemController {
+package me.jihyun.jpashop.Controller;
+
+import lombok.RequiredArgsConstructor;
+import me.jihyun.jpashop.domain.Item.Book;
+import me.jihyun.jpashop.service.ItemService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping("items/new")
+    public String createForm(Model model) {
+        model.addAttribute("form", new BookForm());
+        return "items/createItemForm";
+    }
+
+    @PostMapping("items/new")
+    public String create(BookForm form) {
+        Book book = new Book();
+        /*
+        setter를 제거하고 Book.createBook 메소드를 만들어 리턴 받는 것이 더 좋은 설계
+         */
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setISBN(form.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/";
+
+
+    }
 }
